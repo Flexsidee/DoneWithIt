3f2f7488a393fe06fd16ca27ca11175783f52e62
flexsidee <flexsidee@gmail.com>
2023-06-22 20:06:53 +0100

    designed app textInput

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { Text, View } from "react-native";
 
 import WelcomeScreen from "./app/screen/WelcomeScreen";
 import ViewImageScreen from "./app/screen/ViewImageScreen";
-import Card from "./app/components/Card";
+import AccountScreen from "./app/screen/AccountScreen";
+import ListingsScreen from "./app/screen/ListingsScreen";
 import ListingDetailsScreen from "./app/screen/ListingDetailsScreen";
 import MessagesScreen from "./app/screen/MessagesScreen";
 import Screens from "./app/components/Screens";
-import Icon from "./app/components/Icon";
-import colors from "./app/config/colors";
+import Card from "./app/components/Card";
 import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screen/AccountScreen";
-import ListingsScreen from "./app/screen/ListingsScreen";
+import colors from "./app/config/colors";
+import Icon from "./app/components/Icon";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
+	const [firstName, setFirstName] = useState("");
 	return (
 		<>
+			<Screens>
+				<AppTextInput placeholder="Username" icon="email" />
+			</Screens>
 			{/* <WelcomeScreen /> */}
 			{/* <ViewImageScreen /> */}
 			{/* <ListingDetailsScreen /> */}
 			{/* <MessagesScreen /> */}
 			{/* <AccountScreen /> */}
-			<ListingsScreen />
+			{/* <ListingsScreen /> */}
 		</>
 	);
 }
