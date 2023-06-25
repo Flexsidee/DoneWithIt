a6141805583dcf70072e15672f3e3fd976549b23
flexsidee <flexsidee@gmail.com>
2023-06-25 21:08:06 +0100

    latest update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text, View } from "react-native";
+import { Switch, Text, View } from "react-native";
 
 import WelcomeScreen from "./app/screen/WelcomeScreen";
 import ViewImageScreen from "./app/screen/ViewImageScreen";
@@ -13,13 +13,16 @@ import ListItem from "./app/components/ListItem";
 import colors from "./app/config/colors";
 import Icon from "./app/components/Icon";
 import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
 
 export default function App() {
-	const [firstName, setFirstName] = useState("");
+	const [isNew, setIsNew] = useState(false);
 	return (
 		<>
 			<Screens>
-				<AppTextInput placeholder="Username" icon="email" />
+				<Switch value={isNew} onValueChange={(newVal) => setIsNew(newVal)} />
+				<AppTextInput placeholder="Text" icon="email" />
+				<AppPicker placeholder="This is hte" icon="apps" />
 			</Screens>
 			{/* <WelcomeScreen /> */}
 			{/* <ViewImageScreen /> */}
