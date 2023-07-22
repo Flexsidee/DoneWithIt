aa6a902b7ceb1ea5e5e376647f7b1186b69bbcbd
flexsidee <flexsidee@gmail.com>
2023-07-22 22:17:26 +0100

    ui fixes on ImageInputList

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,32 +1,31 @@
-import { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { useRef } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 import ImageInput from "./forms/ImageInput";
 
 const ImageInputList = ({ imageUris = [], onAddImage, onRemoveImage }) => {
-	const [imageUri, setImageUri] = useState();
-
-	const requestPermission = async () => {
-		const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-		if (!granted) {
-			alert("You need to enable permission to access the library");
-		}
-	};
-
-	useEffect(() => {
-		requestPermission();
-	}, []);
+	const scrollView = useRef();
 
 	return (
-		<View style={styles.container}>
-			{imageUris.map((uri, index) => (
-				<View key={index} style={styles.image}>
-					<ImageInput imageUri={uri} onChangeImage={() => onRemoveImage(uri)} />
+		<View>
+			<ScrollView
+				ref={scrollView}
+				horizontal
+				onContentSizeChange={() => scrollView.current.scrollToEnd()}
+			>
+				<View style={styles.container}>
+					{imageUris.map((uri, index) => (
+						<View key={index} style={styles.image}>
+							<ImageInput
+								imageUri={uri}
+								onChangeImage={() => onRemoveImage(uri)}
+							/>
+						</View>
+					))}
+					<ImageInput onChangeImage={(uri) => onAddImage(uri)} />
 				</View>
-			))}
-			<ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+			</ScrollView>
 		</View>
 	);
 };
